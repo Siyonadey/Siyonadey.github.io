ab47c01bf1629656aea921dd2543fb6653347217
Siyonadey <somnath.dey@gmail.com>
2021-05-23 19:59:17 +0530

    Create success.php

---
+++ Signature/success.php
@@ -0,0 +1,137 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script>
+function copyToClip(str) {
+   str = "<html> <head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></head>" + str + "</body></html>" ;
+    function listener(e) {
+    e.clipboardData.setData("text/html", str);
+    e.clipboardData.setData("text/plain", str);
+    e.preventDefault();
+  }
+  document.addEventListener("copy", listener);
+  document.execCommand("copy");
+  document.removeEventListener("copy", listener);
+};
+</script>
+<title></title>
+</head>
+<body>
+<!-- [if gt mso 14]>
+<style type="text/css" media="all">
+table, tr, td {border-collapse: collapse;}
+tr{ font-size:0px; line-height:0px; border-collapse : collapse;}
+</style>
+<![endif] -->
+<div id="mydata">
+<table  class="wrapper" style="
+    width: 100%;
+    min-height: 120px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    padding: 20px 0;
+    padding-bottom: 60px;
+    color: #000;">
+    <tr>
+    <td>
+    
+
+	        
+		<table align="left" class="image" style=" display: inline-block; margin-right: 20px; margin-bottom: 30px; vertical-align: top; float:left;">
+         <tr>
+              <td width="100%"> <a href="https://shakambhariispat.in" target="_blank" style="color: #000; text-decoration: none;"><img src="https://github.com/Siyonadey/Siyonadey.github.io/blob/master/SIPL_logo.png" nosend="1" alt="SIPL"></a><br>
+           </td>
+        </tr>
+	 </table>
+	 <table  align="left" class="text" style="
+                        display: inline-block;
+                        max-width: 400px;
+                        margin-right: 10px;
+                        padding-left:10px;
+                        border-left: 1px solid;
+                        border-left-color:#39F;
+                        padding-right:10px;
+                        border-right: 1px solid;
+                        border-right-color:#39F;
+                        font-family: Calibri, Lucida Grande, Arial, sans-serif;
+                        font-size: 13px;
+                        line-height: 1.5; float:left;">
+
+			<tr>
+              <td width="100%"> 
+              <h2 style="font-size: 16px;margin: 0;line-height: 1; font-weight: bold;color: #000;"><?php echo trim($_POST['name']); ?></h2>
+                 <span class="subheading" style="color: #000;font-size: 15px; padding-bottom:15px;"> 
+                   <?php echo trim($_POST['user_designation']); ?>, Shakambhari Group </span>    
+                 <span style="height:20px;"> &nbsp; </span>           
+			<br>
+           
+           	
+			<a href="tel" style="color: #000; text-decoration: none;">T: <?php echo trim($_POST['tel_number']); ?>,</a>&nbsp;&nbsp;&nbsp; <a href="tel" style="color: #000; text-decoration: none;">D: <?php echo trim($_POST['desk_number']); ?>,</a>
+			<br>
+            <a href="tel+91" style="color: #000; text-decoration: none;">M: + 91 <?php echo trim($_POST['mobile_number']); ?>,</a>&nbsp;&nbsp;&nbsp; <a href="mailto:<?php echo trim($_POST['email_address']); ?>," style="color: #000; text-decoration: none;">E: <?php echo trim($_POST['email_address']); ?>,</a>
+			<br>
+                      
+			<br>
+           </td>
+           </tr>
+		</table>
+        <table class="social" style="
+                display: inline-block;
+                vertical-align: bottom;
+                max-width: 200px;
+                padding-right: 40px;
+                vertical-align: top;
+                padding-left:10px;
+                float:left;
+        	">	
+            <tr>
+                <td width="100%" align="left" >
+                <!--<a href="http://Shakambhariispat.com" target="_blank" title="" style="display: inline-block;">
+                    <img src="Thermocon_TMT.png" nosend="1" alt="Thermocon TMT" style="vertical-align: middle;">
+                </a>-->
+                 <br>
+                       <table class="text1" style="			
+                                font-family: Calibri, Lucida Grande, Arial, sans-serif;
+                                font-size: 13px;
+                                line-height: 1.5;">
+                         <tr>
+                          <td>
+                                    <a href="http://Shakambariispat.com" target="_blank" title="Shakambhari" style="color:#000; text-decoration: none;">www.Shakambhariispat.com</a>         
+                           <br>    
+                           <br>    
+                   
+                           <a href="https://www.facebook.com/ThermoconTMT/" nosend="1" target="_blank" title="Facebook" style="display: inline-block;text-align: center;margin-right: 7px;margin-top: 4px;">
+                            <img src="https://github.com/Siyonadey/Siyonadey.github.io/blob/master/facebook.png" alt="Facebook"  style="vertical-align: middle;"></a>&nbsp;
+                            <!--<a href="https://www.pinterest.co.uk/ThermoconTMT/" nosend="1" target="_blank" title="pinterest" style="display: inline-block;text-align: center;margin-right: 7px;margin-top: 4px;">
+                            <img src="icon/pinterest.png" alt="pinterest"  style="vertical-align: middle;"></a>&nbsp;-->
+                            <a href="https://thermocontmt.blogspot.com/" nosend="1" target="_blank" title="blogspot" style="display: inline-block;text-align: center;margin-right: 7px;margin-top: 4px;">
+                            <img src="https://github.com/Siyonadey/Siyonadey.github.io/blob/master/blogger.jpg" alt="blogger"  style="vertical-align: middle;"></a>&nbsp;
+                        <!--<a href="https://twitter.com/Spencers_Retail?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" nosend="1" target="_blank" title="Twitter" style="display: inline-block;text-align: center;margin-right: 7px;margin-top: 4px;">
+                            <img src="icon/twitter.png" alt="Twitter"  style="vertical-align: middle;"></a>	&nbsp;
+                
+                        <a href="https://in.linkedin.com/company/spencersretail" nosend="1" target="_blank" title="Linkedin" style="display: inline-block;text-align: center;margin-right: 7px;margin-top: 4px;">                            
+                            <img src="icon/linkedin.png" alt="Linkedin"  style="vertical-align: middle;"></a>
+                             &nbsp;
+                        <a href="https://www.instagram.com/spencersretail/?hl=en" nosend="1" target="_blank" title="Instagram" style="display: inline-block;text-align: center;margin-right: 7px;margin-top: 4px;">                            
+                            <img src="icon/instagram.png" alt="Instagram"  style="vertical-align: middle;"></a>			
+                        <br>-->
+                      
+                    
+                    </td>
+                    </tr>
+                </table>
+               </td>
+           </tr>
+       </table>
+	</td>
+    </tr>
+  </table>
+
+</div>
+<div align="center"><button onclick="copyToClip(document.getElementById('mydata').innerHTML)"> COPY </button> </div>
+
+
+</body>
+</html>
+
